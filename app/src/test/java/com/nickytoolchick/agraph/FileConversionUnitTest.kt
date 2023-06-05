66b60f9312c43445bce8af8080902a296fa2eb0d
Nikita Sutulov <notsid212@gmail.com>
2023-06-05 23:43:11 +0300

    added more unit tests for file conversion
    Added two more cases for testing object conversion to format so that the tests now show that the app works correctly in more different cases.

--- app/src/test/java/com/nickytoolchick/agraph/FileConversionUnitTest.kt
+++ app/src/test/java/com/nickytoolchick/agraph/FileConversionUnitTest.kt
@@ -25,4 +25,27 @@ class FileConversionUnitTest {
         val expectedConvertedFileString = """{"verticalStep":7.0}${Constants.SPLITTER}{"points":[],"isSmooth":true}"""
         assertEquals(expectedConvertedFileString, fileWriter.convertToChartFile(changedChartOptions, changedDatasetOptions))
     }
+
+    @Test
+    fun checkChartWithBooleanParameters() {
+        val changedChartOptions = ChartOptions().apply {
+            isLogScaleX = true
+            isLogScaleY = true
+            isHorizontalLines = true
+            isVerticalLines = true
+        }
+        val changedDatasetOptions = DatasetOptions().apply { isSmooth = true }
+        val fileWriter = FileWriter()
+        val expectedConvertedFileString = """{"isLogScaleX":true,"isLogScaleY":true,"isHorizontalLines":true,"isVerticalLines":true}${Constants.SPLITTER}{"points":[],"isSmooth":true}"""
+        assertEquals(expectedConvertedFileString, fileWriter.convertToChartFile(changedChartOptions, changedDatasetOptions))
+    }
+
+    @Test
+    fun checkChartWithPoints() {
+        val changedChartOptions = ChartOptions()
+        val changedDatasetOptions = DatasetOptions().apply { points = arrayOf(Pair(2.7f, 3.9f)) }
+        val fileWriter = FileWriter()
+        val expectedConvertedFileString = """{}${Constants.SPLITTER}{"points":[{"first":2.7,"second":3.9}]}"""
+        assertEquals(expectedConvertedFileString, fileWriter.convertToChartFile(changedChartOptions, changedDatasetOptions))
+    }
 }
