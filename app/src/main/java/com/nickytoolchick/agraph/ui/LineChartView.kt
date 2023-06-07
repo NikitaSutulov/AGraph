a069738c37df43fb9608f1c66293a9ec0d6fe5c5
Nikita Sutulov <notsid212@gmail.com>
2023-06-08 00:18:34 +0300

    Delete LineChartView.kt
    Deleted unused class LineChartView that wasn't even implemented to avoid possible confusion and make the code cleaner

--- app/src/main/java/com/nickytoolchick/agraph/ui/LineChartView.kt
+++
@@ -1,7 +0,0 @@
-package com.nickytoolchick.agraph.ui
-
-// This class will be used to draw the chart with the coordinates plane on it
-// However, it's not implemented yet
-class LineChartView {
-    // TODO("Not implemented yet")
-}
