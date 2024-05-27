e565672c8877a4f7fc452a9817d4eaa33996048f
Nikita Sutulov <notsid212@gmail.com>
2024-05-27 16:35:31 +0300

    refactored MainActivity.kt
    Refactored the contents of MainActivity.kt for the code to be cleaner.

--- app/src/main/java/com/nickytoolchick/agraph/MainActivity.kt
+++ app/src/main/java/com/nickytoolchick/agraph/MainActivity.kt
@@ -33,50 +33,80 @@ class MainActivity : AppCompatActivity() {
 
     private fun setButtonsOnClickListeners() {
         binding.configureChartButton.setOnClickListener {
-            val charOptionsIntent = Intent(this, ChartOptionsActivity::class.java)
-            charOptionsIntent.putExtra(Constants.STABLE_CHART_OPTIONS, Json.encodeToString(chartOptions))
-            chartOptionsResultLauncher.launch(charOptionsIntent)
+            startChartOptionsActivity()
         }
         binding.configureDatasetButton.setOnClickListener {
-            val datasetOptionsIntent = Intent(this, DatasetOptionsActivity::class.java)
-            datasetOptionsIntent.putExtra(Constants.STABLE_DATASET_OPTIONS, Json.encodeToString(datasetOptions))
-            datasetOptionsResultLauncher.launch(datasetOptionsIntent)
+            startDatasetOptionsActivity()
         }
         binding.renderChartButton.setOnClickListener {
-            val chartRenderIntent = Intent(this, ChartRenderActivity::class.java).apply {
-                putExtra(Constants.STABLE_CHART_OPTIONS, Json.encodeToString(chartOptions))
-                putExtra(Constants.STABLE_DATASET_OPTIONS, Json.encodeToString(datasetOptions))
-            }
-            startActivity(chartRenderIntent)
+            startChartRenderActivity()
         }
         binding.saveButton.setOnClickListener {
-            val fileWriter = FileWriter()
-            fileWriter.saveFile(this, chartOptions, datasetOptions)
+            saveFile()
         }
         binding.loadButton.setOnClickListener {
-            val fileReader = FileReader()
-            val loadedOptions = fileReader.readFile(this)
-            chartOptions = loadedOptions.first
-            datasetOptions = loadedOptions.second
-            validateOptions()
+            loadFile()
         }
     }
 
-    private val chartOptionsResultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
-        if (result.resultCode == Activity.RESULT_OK) {
-            chartOptions = Json.decodeFromString(result.data?.getStringExtra(Constants.NEW_CHART_OPTIONS)!!)
-            validateOptions()
-        }
+    private fun startChartOptionsActivity() {
+        val charOptionsIntent = Intent(this, ChartOptionsActivity::class.java)
+        charOptionsIntent.putExtra(
+            Constants.STABLE_CHART_OPTIONS,
+            Json.encodeToString(chartOptions)
+        )
+        chartOptionsResultLauncher.launch(charOptionsIntent)
     }
 
-    private val datasetOptionsResultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
-        if (result.resultCode == Activity.RESULT_OK) {
-            datasetOptions = Json.decodeFromString(result.data?.getStringExtra(Constants.NEW_DATASET_OPTIONS)!!)
-            validateOptions()
-        }
+    private fun startDatasetOptionsActivity() {
+        val datasetOptionsIntent = Intent(this, DatasetOptionsActivity::class.java)
+        datasetOptionsIntent.putExtra(
+            Constants.STABLE_DATASET_OPTIONS,
+            Json.encodeToString(datasetOptions)
+        )
+        datasetOptionsResultLauncher.launch(datasetOptionsIntent)
     }
+
+    private fun startChartRenderActivity() {
+        val chartRenderIntent = Intent(this, ChartRenderActivity::class.java).apply {
+            putExtra(Constants.STABLE_CHART_OPTIONS, Json.encodeToString(chartOptions))
+            putExtra(Constants.STABLE_DATASET_OPTIONS, Json.encodeToString(datasetOptions))
+        }
+        startActivity(chartRenderIntent)
+    }
+
+    private fun saveFile() {
+        val fileWriter = FileWriter()
+        fileWriter.saveFile(this, chartOptions, datasetOptions)
+    }
+
+    private fun loadFile() {
+        val fileReader = FileReader()
+        val loadedOptions = fileReader.readFile(this)
+        chartOptions = loadedOptions.first
+        datasetOptions = loadedOptions.second
+        validateOptions()
+    }
+
+    private val chartOptionsResultLauncher =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                chartOptions =
+                    Json.decodeFromString(result.data?.getStringExtra(Constants.NEW_CHART_OPTIONS)!!)
+                validateOptions()
+            }
+        }
+
+    private val datasetOptionsResultLauncher =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                datasetOptions =
+                    Json.decodeFromString(result.data?.getStringExtra(Constants.NEW_DATASET_OPTIONS)!!)
+                validateOptions()
+            }
+        }
 
     private fun validateOptions() {
-        binding.renderChartButton.isEnabled = (datasetOptions.points.isNotEmpty())
+        binding.renderChartButton.isEnabled = datasetOptions.points.isNotEmpty()
     }
 }
