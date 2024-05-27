3846635a307d28f4ae6ba80771898b9e2c875473
Nikita Sutulov <notsid212@gmail.com>
2024-05-27 16:12:24 +0300

    refactored ChartRenderActivity.kt
    Refactored the contents of ChartRenderActivity.kt for the code to be cleaner.

--- app/src/main/java/com/nickytoolchick/agraph/ui/ChartRenderActivity.kt
+++ app/src/main/java/com/nickytoolchick/agraph/ui/ChartRenderActivity.kt
@@ -3,7 +3,6 @@ package com.nickytoolchick.agraph.ui
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.LinearLayout
 import com.nickytoolchick.agraph.data.ChartOptions
 import com.nickytoolchick.agraph.data.Constants
 import com.nickytoolchick.agraph.data.DatasetOptions
@@ -15,38 +14,41 @@ import kotlinx.serialization.json.Json
 
 class ChartRenderActivity : AppCompatActivity() {
 
-    var chartOptions = ChartOptions()
-    var datasetOptions = DatasetOptions()
-    lateinit var mainActivityIntent: Intent
+    private var chartOptions = ChartOptions()
+    private var datasetOptions = DatasetOptions()
+    private lateinit var mainActivityIntent: Intent
     private lateinit var binding: ActivityChartRenderBinding
-
     private val fileWriter = FileWriter()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityChartRenderBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         mainActivityIntent = intent
         loadOptions()
         initChartRenderer()
-        setContentView(binding.root)
-
-        binding.exportButton.setOnClickListener {
-            fileWriter.exportChartAsPng(binding.chartRenderer)
-        }
-
-        binding.shareButton.setOnClickListener {
-            fileWriter.shareChart(this, binding.chartRenderer)
-        }
+        setupButtonListeners()
     }
 
     private fun initChartRenderer() {
-        binding.chartRenderer.setChartOptions(chartOptions)
-        binding.chartRenderer.setDatasetOptions(datasetOptions)
+        binding.chartRenderer.apply {
+            setChartOptions(chartOptions)
+            setDatasetOptions(datasetOptions)
+        }
     }
 
     private fun loadOptions() {
         chartOptions = Json.decodeFromString(mainActivityIntent.getStringExtra(Constants.STABLE_CHART_OPTIONS)!!)
         datasetOptions = Json.decodeFromString(mainActivityIntent.getStringExtra(Constants.STABLE_DATASET_OPTIONS)!!)
+    }
+
+    private fun setupButtonListeners() {
+        binding.exportButton.setOnClickListener {
+            fileWriter.exportChartAsPng(binding.chartRenderer)
+        }
+        binding.shareButton.setOnClickListener {
+            fileWriter.shareChart(this, binding.chartRenderer)
+        }
     }
 }
